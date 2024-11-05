650374b6e03fbdf4c91bf8c9c3b09777438530f7
aashishpanchal <aipanchal51@gmail.com>
2024-11-05 16:14:39 +0530

    my config

---
+++ linux/.bashrc
@@ -0,0 +1,18 @@
+# Add: Custom command
+alias g='git'
+alias cls='clear'
+alias del='rm -r'
+alias reload='source ~/.bashrc'
+alias ls='exa -la --icons -s inode' # use exa instead of ls
+
+# Add: oh-my-posh, https://github.com/jandedobbeleer/oh-my-posh
+eval "$(oh-my-posh init bash --config ~/.cache/oh-my-posh/themes/space.omp.json)" # load oh-my-posh
+
+# Add: JDK Env
+export JAVA_HOME=/opt/openjdk/jdk-17.0.13+11
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Add: Android Env
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
---
+++ windows/profile.ps1
@@ -0,0 +1,17 @@
+# oh-my-posh, https://github.com/jandedobbeleer/oh-my-posh
+oh-my-posh init pwsh --config '~\scoop\apps\oh-my-posh\current\themes\catppuccin_latte.omp.json' | Invoke-Expression
+
+# alias
+Set-Alias g git # git
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# ps-read-line, https://github.com/PowerShell/PSReadLine
+Set-PSReadLineOption -EditMode emacs # windows | emacs | vi
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+# Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+
+# utils function
+function ll {eza -l --icons $args} # check-out https://github.com/eza-community/eza
+function whereis ($command) {(Get-Command -Name $command -ErrorAction SilentlyContinue).Path}
